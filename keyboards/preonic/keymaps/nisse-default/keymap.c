4169b9cb5c0aaa8e276481d116ac336bb4893983
nilssab <nilssab@gmail.com>
2017-06-02 09:45:07 +0900

    keymap changes

--- keyboards/preonic/keymaps/nisse-default/keymap.c
+++ keyboards/preonic/keymaps/nisse-default/keymap.c
@@ -101,11 +101,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |      | M-<  | PGUP |  UP  | PGDN | M->  |  +   |   7  |   8  |  9   |   .  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      | HOME | LEFT | DOWN |RIGHT | END  |  -   |   4  |   5  |  6   |   0  |Enter |
+ * |   `  | HOME | LEFT | DOWN |RIGHT | END  |  -   |   4  |   5  |  6   |   0  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |  $   |   %  |   @  |  |   |  =   |  _   |   1  |   2  |  3   |   ,  | Del  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Next |      | PGDN | Play |
+ * |      |      |      |      |      |             |      |      |      | PGDN | Play |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = {
@@ -113,7 +113,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {_______,LALT(S(KC_COMM)),KC_PGUP,KC_UP,KC_PGDN,LALT(S(KC_DOT)),KC_PLUS,KC_7,    KC_8,   KC_9,    KC_DOT,  KC_BSPC},
   {KC_GRV,  KC_HOME,    KC_LEFT, KC_DOWN, KC_RIGHT, KC_END,  KC_MINS,     KC_4,    KC_5,   KC_6,    KC_0,    KC_ENTER},
   {_______, KC_DLR,     KC_PERC, KC_AT,   KC_PIPE,  KC_EQL,  KC_UNDS,     KC_1,    KC_2,   KC_3,    KC_COMM, KC_DEL},
-  {_______, _______,    _______, _______, _______, _______, _______, _______, _______, KC_PGUP, KC_PGDN, KC_MPLY}
+  {_______, _______,    _______, _______, _______,  _______, _______,     _______, _______,KC_MPRV, KC_MNXT, KC_MPLY}
 },
 
 /* Raise
@@ -122,18 +122,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |MWhUp |  MB4 | MUp  |  MB3 |   !  |  \   |   [  |   ]  | MB1  |  MB2 | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |MWhDwn|MLeft |MDown |MRight|   &  |  '   |   (  |   )  |   {  |   }  | ENTR |
+ * | CAPS |MWhDwn|MLeft |MDown |MRight|   &  |  '   |   (  |   )  |   {  |   }  | ENTR |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      | MB5  |PrScrn|  ^   |  #   |   ~  |  *   |   "  |   ;  |   :  | Vol+ | Del  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Next |      | Vol- | Play |
+ * |      |      |      |      |      |             |      |      |      | Vol- | Play |
  * `-----------------------------------------------------------------------------------'
  */
 
 [_RAISE] = {
   {KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,    KC_F8,    KC_F9,   KC_F10,     KC_F11,    KC_F12},
   {_______, KC_WH_U, KC_BTN4, KC_MS_U, KC_BTN3, KC_EXLM, KC_BSLASH,KC_LBRC,  KC_RBRC, KC_BTN1,    KC_BTN2,   KC_BSPC},
-  {_______, KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, KC_AMPR, KC_QUOT,  KC_LPRN,  KC_RPRN, S(KC_LBRC), S(KC_RBRC),KC_ENTER},
+  {KC_CAPS, KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, KC_AMPR, KC_QUOT,  KC_LPRN,  KC_RPRN, S(KC_LBRC), S(KC_RBRC),KC_ENTER},
   {_______, KC_BTN5, KC_PSCR, KC_CIRC, KC_HASH, KC_TILD, KC_ASTR,  KC_DQUO,  KC_SCLN, S(KC_SCLN), KC_VOLU,   KC_DEL},
   {_______, _______, _______, _______, _______, _______, _______,  _______,  _______, KC_MUTE,    KC_VOLD,   KC_MPLY}
 },
