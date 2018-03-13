745d7ecc5880c31b743da5a3d0be2968973feb33
nilssab <nilssab@gmail.com>
2018-03-14 00:19:04 +0900

    added arrows to default map

--- keyboards/planck/keymaps/nisse-default/keymap.c
+++ keyboards/planck/keymaps/nisse-default/keymap.c
@@ -47,16 +47,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |  Tab |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |LShift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |RShift|
+ * |LShift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |  up  | DEL  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |  GUI | Ctrl | RAlt |Lower |    Space    | Raise| Ctrl | LAlt |  GUI | Del  |
+ * | Ctrl |  GUI | RAlt | Ctrl |Lower |    Space    | Raise| Alt  | left | down | right|
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
   {S(KC_TAB), KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC},
   {KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_ENT},
-  {KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT},
-  {KC_LCTL,   KC_LGUI, KC_LCTL, KC_RALT, LOWER, KC_SPC, KC_SPC, RAISE, KC_LCTL, KC_LALT, KC_LGUI, KC_DEL}
+  {KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_UP,   KC_DEL},
+  {KC_LCTL,   KC_LGUI, KC_RALT, KC_LCTL, LOWER, KC_SPC, KC_SPC, RAISE, KC_LALT, KC_LEFT, KC_DOWN, KC_RIGHT}
 },
 
 /* Lower
