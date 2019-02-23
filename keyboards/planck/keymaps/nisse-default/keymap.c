21af379d9dd9ce673b0322ce1d62525b67fb3028
Nils Sabelstrom <nilssab@gmail.com>
2019-02-24 00:16:35 +0900

    removed shift space on planck

--- keyboards/planck/keymaps/nisse-default/keymap.c
+++ keyboards/planck/keymaps/nisse-default/keymap.c
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {S(KC_TAB),KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,  KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC},
   {KC_TAB,   KC_A,    KC_S,    KC_D,    KC_F,  KC_G,  KC_H,   KC_J,  KC_K,    KC_L,    KC_SLSH, KC_ENT},
   {KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,  KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_UP,   KC_DEL},
-  {KC_LCTL,  KC_LGUI, KC_RALT, KC_LALT, LOWER, ShSpc, ShSpc,  RAISE, KC_RCTL, KC_LEFT, KC_DOWN, KC_RIGHT}
+  {KC_LCTL,  KC_LGUI, KC_RALT, KC_LALT, LOWER, KC_SPC,KC_SPC,  RAISE, KC_RCTL, KC_LEFT, KC_DOWN, KC_RIGHT}
 },
 
 /* Lower
