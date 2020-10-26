d798c257faa6b9cdf9e9265fa8a9c0f1695d96a0
Nils Sabelström <nilssab@gmail.com>
2020-10-26 09:55:31 +0900

    Update keymap.c
    fixed typo in keymap

--- keyboards/planck/keymaps/nisse-default/keymap.c
+++ keyboards/planck/keymaps/nisse-default/keymap.c
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {S(KC_TAB),KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,  KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC},
   {KC_TAB,   KC_A,    KC_S,    KC_D,    KC_F,  KC_G,  KC_H,   KC_J,  KC_K,    KC_L,    KC_SLSH, KC_ENT},
   {KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,  KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_UP,   KC_DEL},
-  {KC_LCTL,  KC_LGUI, KC_RALT, KC_LALT, LOWER, ShSPC, ShSPC,  RAISE, KC_RCTL, KC_LEFT, KC_DOWN, KC_RIGHT}
+  {KC_LCTL,  KC_LGUI, KC_RALT, KC_LALT, LOWER, ShSpc, ShSpc,  RAISE, KC_RCTL, KC_LEFT, KC_DOWN, KC_RIGHT}
 },
 [_QWERTYNoShSpc] = {
   {S(KC_TAB),KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,  KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC},
@@ -100,7 +100,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_CAPS, KC_WH_U, KC_WH_L, KC_MS_U, KC_WH_R, KC_TILD, KC_GRV,  KC_LBRC, KC_RBRC, KC_BTN1,    KC_BTN2,   KC_BTN3},
   {KC_ESC,  KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, KC_EXLM, KC_QUOT, KC_LPRN, KC_RPRN, S(KC_LBRC), S(KC_RBRC),KC_BTN4},
   {_______, KC_PAUSE,KC_PSCR, KC_HASH, KC_PIPE, KC_AMPR, KC_DQUO, KC_AT,   KC_SCLN, S(KC_SCLN), KC_BSLASH, KC_BTN5},
-  {_______, _______, _______, _______, _______, ShSpc,   ShSpc,   _______, _______, KC_MUTE,    KC_VOLD,   KC_VOLU}
+  {_______, _______, _______, _______, _______, KC_SPC,  KC_SPC,  _______, _______, KC_MUTE,    KC_VOLD,   KC_VOLU}
 },
 
 /* Adjust (Lower + Raise)
